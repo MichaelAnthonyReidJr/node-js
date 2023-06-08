918a268b1977cef4932a27b33b84cfef5e354d79
Michael Reid <EfficiencyMarriesEfficacy@gmail.com>
2023-06-08 12:09:26 -0400

    test

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const { body, check, param, validationResult } = require("express-validator");
 const { promisePool } = require("./PromisePool.js");
-const mySqlProxy  = require('./mySqlProxy.js')
+const mySqlProxy = require("./mySqlProxy.js");
 const PORT = 80;
 const app = express();
 const corsOptions = {
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Your endpoints here..
+//Exercise 1 Hello World
 app.get("/message", cors(corsOptions), async (req, res) => {
   // const result = await mySqlProxy.<YOUR FUNCTION HERE>
   // const id = req.params['id']                  // Read parameters from URL.
@@ -24,55 +24,105 @@ app.get("/message", cors(corsOptions), async (req, res) => {
   // res.send(<YOUR OBJECT HERE>)
   res.send({ message: "Hello World" });
 });
+
+//Exercise 2 Get Car
 app.get(`/car/:id`, cors(corsOptions), async (req, res) => {
-  const id = req.params.id; 
-  const [ result ] = await promisePool.query("SELECT * FROM car WHERE car_id = ? ", [id]);
-  res.send( result [0]);
- });
-app.get('/person/:id', cors(corsOptions), async (req, res) => {
+  const id = req.params.id;
+  const [result] = await promisePool.query(
+    "SELECT * FROM car WHERE car_id = ? ",
+    [id]
+  );
+  res.send(result[0]);
+});
+
+//Exercise 3 Get Car for make
+app.get("/cars", cors(corsOptions), async (req, res) => {
+  const make = req.params.make;
+  const [result] = await promisePool.query("SELECT * FROM car WHERE make = ?", [
+    make,
+  ]);
+  res.send(result);
+});
+
+//Exercise 4 Post car
+app.post("/cars/", cors(corsOptions), async (req, res) => {
+  try {
+    const newCAr = req.body;
+    console.log(newCAr);
+    const [result] = await promisePool.execute("INSERT INTO car(make, model, color, price) VALUES(?, ?, ?, ?)", [newCAr.make , newCar.model, newCar.color, newCar.price]);
+    
+    const newCarId = result.insertId;
+    const [newCar] = await promisePool.query(
+      "SELECT * FROM car WHERE car_id = ?",
+      newCarId
+    );
+    //res.send("Car has been added")
+    res.send(newCar[0]);
+  } catch {
+    res.status(500).send("Error.....adding car");
+  }
+});
+
+//Exercise 5 Put Car
+app.put("/cars/:id", cors(corsOptions), async (req, res) => {
+  const carID = req.params.id;
+  const [result] = await promisePool.query(
+    "UPDATE car SET make =?, model =?, price = ?, color = ?",
+    ["Mazda", "Mazda6", 30000, "silver"]
+  );
+  res.send({ message: result.info });
+});
+
+//Exercise 6 Delete Car
+app.delete("/cars/:id", cors(corsOptions), async (req, res) => {
+  const caRID = req.params.id;
+  const [result] = await promisePool.query("DELETE FROM car WHERE car_id = ?", [
+    caRID,
+  ]);
+  console.log("Deleted!");
+  res.send(result);
+});
+
+//Exercise 7 Harden Car Endpoints
+app.delete("cars/:id", cors(corsOptions), async (req, res) => {
+  try {
+    const caRID = req.params.id;
+    const [result] = await promisePool.query(
+      "DELETE FROM car WHERE car_id = ?",
+      [caRID]
+    );
+    console.log("Deleted!");
+    res.send(result);
+  } catch {
+    res.status(404).send("Error....File not found");
+  }
+  const caRID = req.params.id;
+  const [result] = await promisePool.query("DELETE FROM car WHERE car_id = ?", [
+    caRID,
+  ]);
+  console.log("Deleted!");
+  res.send(result);
+});
+
+//TLMS - Express API Part II Using mySQLPrroxy
+app.get("/person/:id", cors(corsOptions), async (req, res) => {
   const personID = req.params.id;
   const person = await mySqlProxy.selectPersonById(personID);
-  res.send(person)
+  res.send(person);
+});
+//TLMS - Express API Part III Using mySQLPrroxy
+app.post("/person/", cors(corsOptions), async (req, res) => {
+  const person1 = req.body;
+  const addedPerson = await mySqlProxy.insertPerson(person1);
+  res.send(addedPerson);
 });
 
+//Practice
 app.get(`/person`, cors(corsOptions), async (req, res) => {
-   const people = await promisePool.query('SELECT * FROM person');
-   res.send(people)
-});
-app.get('/cars', cors(corsOptions), async (req, res) => {
-  const make = req.params.make;
-  console.log(make);
-  const [ result ]= await promisePool.query('SELECT * FROM car WHERE make = ?',  [ make ]);
-  res.send(result);
-});
-app.put('/cars/:id', cors(corsOptions), async (req,res) => {
-  const carID = req.params.id;
-  const [ result ] = await promisePool.query('UPDATE car SET make =?, model =?, price = ?, color = ?'
-  , ["Mazda", "Mazda6", 30000, "silver"]);
-  res.send({"message": result.info})
-});
-app.delete('/cars/:id', cors(corsOptions), async(req, res) => {
-  const caRID = req.params.id;
-  const [ result ] = await promisePool.query('DELETE FROM car WHERE car_id = ?', [caRID])
-  console.log("Deleted!")
-  res.send(result);
-});
-
-app.post('/cars/', cors(corsOptions), async (req, res) => {
-try{
-  const [ result ]  = await promisePool.query('INSERT INTO car(make, model, color, price) VALUES(?, ?, ?, ?)'
-  , ["Toyota","Corolla", "Gray", 30000]);
-  
-  const newCarId = result.insertId;
-  const [ newCar ] = await promisePool.query('SELECT * FROM car WHERE car_id = ?', newCarId)
-  //res.send("Car has been added")
-  res.send(newCar[0]);
-}
-catch {
-  res.status(500).send("Error.....adding car")
-}
+  const people = await promisePool.query("SELECT * FROM person");
+  res.send(people);
 });
 
 app.listen(PORT, () => {
-    console.log(`Express web API running on port: ${PORT}.`)
+  console.log(`Express web API running on port: ${PORT}.`);
 });
--- server/mySqlProxy.js
+++ server/mySqlProxy.js
@@ -1,6 +1,7 @@
 const promisePool = require("./PromisePool").promisePool;
 
 const SELECT_PERSON = "SELECT * FROM person WHERE person_id = ?";
+const INSERT_PERSON = "INSERT into person (first_name, last_name) values (?,?)"
 
 exports.selectPersonById = async (personId) => {
   try {
@@ -11,3 +12,12 @@ exports.selectPersonById = async (personId) => {
     console.log(e);
   }
 };
+exports.insertPerson = async (person) => {
+  try{
+    const [ rows ] = await promisePool.execute(INSERT_PERSON, [person.firstname, person.lastName]);
+    return { personId: rows.insertId, ...person };
+  }
+  catch(e) {
+    console.log(e);
+  }
+}
